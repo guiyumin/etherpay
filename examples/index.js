d6c3027e574f6dd0a23e219e1a2096d1a18a12ef
guiyumin <guiyumin@protonmail.com>
2024-05-16 23:21:19 -0700

    fix: add an example

--- examples/index.js
+++ examples/index.js
@@ -5,20 +5,20 @@ const provider = new ethers.JsonRpcProvider(
   `https://mainnet.infura.io/v3/${INFURA_ID}`
 );
 
+const provider2 = new ethers.JsonRpcProvider(
+  `https://ether-gateway.etherpay.dev/v1/mainnet`
+);
+
 const address = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
 
 const main = async () => {
-  const balance = await provider.getBalance(address);
+  const balance = await provider2.getBalance(address);
   console.log(
     `\nETH Balance of ${address} --> ${ethers.formatEther(balance)} ETH\n`
   );
 };
 
 main();
-
-const provider2 = new ethers.JsonRpcProvider(
-  `https://ether-gateway.etherpay.dev/v1/mainnet`
-);
 
 const main2 = async () => {
   const blockNumber = await provider2.getBlockNumber();
