903cdeb046c9cee6fda4369ecd1dd8e002bb9a74
guiyumin <guiyumin@protonmail.com>
2024-05-16 23:00:27 -0700

    fix: add an example

---
+++ examples/index.js
@@ -0,0 +1,17 @@
+const ethers = require("ethers");
+
+const INFURA_ID = "9bdd3aa48a5145cdb0f8f843faf25f3d";
+const provider = new ethers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${INFURA_ID}`
+);
+
+const address = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
+
+const main = async () => {
+  const balance = await provider.getBalance(address);
+  console.log(
+    `\nETH Balance of ${address} --> ${ethers.formatEther(balance)} ETH\n`
+  );
+};
+
+main();
