ff68fb63f99599e48e89600fef2ceb99cae0bcf4
guiyumin <guiyumin@protonmail.com>
2024-05-19 19:52:31 -0700

    feat: arco design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@arco-design/web-react/dist/css/arco.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
